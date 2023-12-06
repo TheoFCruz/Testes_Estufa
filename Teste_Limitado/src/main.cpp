0561a08698611869d7fef01a1ddd746e86edaed9
Theo Cruz <theofonsecacruz@gmail.com>
2023-12-06 12:10:15 -0300

    Finalizacao teste final com potenciometro

--- Teste_Limitado/src/main.cpp
+++ Teste_Limitado/src/main.cpp
@@ -2,9 +2,26 @@
 #include <DHT.h>
 
 #define LED 21 
-#define POT 33
+#define POT 2 
+
+#define MAX_T_TEMP 40.0
+#define MIN_T_TEMP 0.0
 
 DHT Sensor(18, DHT22);
+
+float analog_to_temp(int analog) {
+	float m = (MAX_T_TEMP - MIN_T_TEMP)/(4095);
+	return (MIN_T_TEMP + m*analog);
+}
+
+void blink_led(float temp_diff) {
+	float blink_delay = 1000/(temp_diff + 1);
+
+	digitalWrite(LED, HIGH);
+	delay(blink_delay);	
+	digitalWrite(LED, LOW);
+	delay(blink_delay);	
+}
 
 void setup(){
 	Serial.begin(115200);
@@ -18,23 +35,20 @@ float current_temp = Sensor.readTemperature();
 float target_temp = 25.0;
 
 void loop(){
-	// //Imprime as informacoes no monitor serial
-	// Serial.print("Temperatura atual: ");
-	// Serial.println(current_temp);
-	// Serial.print("Temperatura alvo: ");
-	// Serial.println(target_temp);
-	//
-	// //Atualiza as temperaturas
-	// if (digitalRead(UP) == 0) target_temp++;
-	// if (digitalRead(DOWN) == 0) target_temp--;
-	//
-	// current_temp = Sensor.readTemperature();
-	//
-	// //Pisca o LED (ou nao)
-	// if (current_temp > target_temp) digitalWrite(LED, HIGH);	
-	// else digitalWrite(LED, LOW);
-	// 
-	// delay(100);
-	Serial.println(analogRead(POT));	
+	//Imprime as informacoes no monitor serial
+	Serial.print("Temperatura atual: ");
+	Serial.println(current_temp);
+	Serial.print("Temperatura alvo: ");
+	Serial.println(target_temp);
+
+	//Atualiza as temperaturas
+	target_temp = analog_to_temp(analogRead(POT));
+
+	current_temp = Sensor.readTemperature();
+
+	//Pisca o LED (ou nao)
+	if (current_temp > target_temp) blink_led(current_temp - target_temp);	
+	else digitalWrite(LED, LOW);
+
 	delay(100);
 }
